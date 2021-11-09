4665d30f87220be8b708b8aff62273adc421f972
spook <hersgori4@protonmail.com>
2021-11-09 21:11:51 +0300

    created SQL schema of my project

---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS doingsdone_NoSpooksAllowed;
+
+USE doingsdone_NoSpooksAllowed;
+
+CREATE TABLE IF NOT EXISTS user(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(70) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    registration_date DATETIME NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS project(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(70) NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS tasks(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    creation_date DATETIME NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT False,
+    file VARCHAR(255),
+    end_time DATETIME NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    project_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (project_id) REFERENCES project(id)
+);
