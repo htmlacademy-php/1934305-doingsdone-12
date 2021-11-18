60c77335b54a49fc8f86278e20e0bb65d8c7ac8c
spook <hersgori4@protonmail.com>
2021-11-18 17:10:16 +0300

    include url.php file in init.php

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php
 $config = require_once("config.php");
 
+require_once("functions/url.php");
 require_once("functions/database.php");
 require_once("functions/validation.php");
 require_once("functions/templates.php");
