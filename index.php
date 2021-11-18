224871aa3be307c6c0cb8d7e6a58d7c3b57a4f02
spook <hersgori4@protonmail.com>
2021-11-18 20:41:22 +0300

    maked new function which gets tasks by projectId, and new SQL query in getProjects function

--- index.php
+++ index.php
@@ -2,22 +2,30 @@
 
 require_once("init.php");
 
-$showCompleteTasks = rand(0, 1);
+$showCompleteTasks = 1;
 $title = "Дела в порядке";
 
 $con = makeConnection($config["db"]);
 
-$id = 1; // Сейчас пока 1, потом заменю на $_GET
+$userId = 1; // Сейчас пока 1, потом заменю на $_GET
+$projectId = $_GET["project_id"] ?? null;
+
 $projects = [];
 
-$projects = getProjects($con, $id);
-$tasks = getTasks($con, $id);
+$projects = getProjects($con, $userId);
+
+if ($projectId) {
+    $tasks = getTasksByProjectId($con, $userId, $projectId);
+} else {
+    $tasks = getTasksAll($con, $userId);
+}
 
 $pageContent = includeTemplate("main.php", [
     "projects" => $projects,
     "tasks" => $tasks,
     "showCompleteTasks" => $showCompleteTasks,
-    "scriptName" => pathinfo(__FILE__, PATHINFO_BASENAME)
+    "scriptName" => pathinfo(__FILE__, PATHINFO_BASENAME),
+    "projectId" => $projectId
 ]);
 
 $layoutContent = includeTemplate("layout.php", [
