b4dee092d0a07f2db771b50ad6e556b9d88dcc42
spook <hersgori4@protonmail.com>
2021-12-01 20:08:35 +0300

    wrote validation test and add a new library to composer

---
+++ tests/Unit/ValidateProjectTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ValidateProjectTest extends TestCase
+{
+    public function testValidateProject()
+    {
+        $this->assertEquals(null, validateProject(1, [1, 2, 3, 4]));
+        $this->assertEquals("Указан несуществующий проект", validateProject(0, [1, 2]));
+    }
+}
