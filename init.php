db7c7851f2ebaead5bf1a0b32285d21b8e05d7ac
spook <hersgori4@protonmail.com>
2021-12-15 17:46:03 +0300

    removed round brackets around require_once call

--- init.php
+++ init.php
@@ -1,11 +1,11 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
-$config = require_once("config.php");
+$config = require_once "config.php";
 
-require_once("functions/url.php");
-require_once("functions/database.php");
-require_once("functions/validation.php");
-require_once("functions/templates.php");
-require_once("functions/misc.php");
+require_once "functions/url.php";
+require_once "functions/database.php";
+require_once "functions/validation.php";
+require_once "functions/templates.php";
+require_once "functions/misc.php";
 
 $con = makeConnection($config["db"]);
