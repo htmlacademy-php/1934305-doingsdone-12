96a9c12ddb5b46a5fe6e4b55027ed922e2b87913
spook <hersgori4@protonmail.com>
2022-01-17 22:38:23 +0300

    finished logic in index.php

--- index.php
+++ index.php
@@ -19,56 +19,24 @@ if (!isset($_SESSION["user"])) {
     exit();
 }
 
-$btnActive = [];
-
-if (!isset($_SESSION["show_complete_tasks"])) {
-    $_SESSION["show_complete_tasks"] = 1;
-}
-
-if (isset($_GET["show_completed"])) {
-    $_SESSION["show_complete_tasks"] = filter_input(INPUT_GET, "show_completed", FILTER_SANITIZE_NUMBER_INT);
-}
-
+saveCompleteTasksToSession();
 $showCompleteTasks = (int) $_SESSION["show_complete_tasks"];
 
-$projectId = filter_input(INPUT_GET, "project_id", FILTER_SANITIZE_NUMBER_INT);
+$queryStringsValues = getQueriesWrapper();
 
-$taskId = filter_input(INPUT_GET, "task_id", FILTER_SANITIZE_NUMBER_INT);
-
-$currentDay = (int) filter_input(INPUT_GET, "current_day", FILTER_SANITIZE_NUMBER_INT);
-$tomorrow = (int) filter_input(INPUT_GET, "tomorrow", FILTER_SANITIZE_NUMBER_INT);
-$overdue = (int) filter_input(INPUT_GET, "overdue", FILTER_SANITIZE_NUMBER_INT);
-
-if ($taskId) {
-    updateStatusTask($con, $userId, $taskId);
+if ($queryStringsValues["task_id"]) {
+    updateStatusTask($con, $userId, $queryStringsValues["task_id"]);
 }
 
 $projects = [];
 
 $projects = getProjects($con, $userId);
 
-if ($projectId) {
-    $tasks = getTasksByProjectId($con, $userId, $projectId);
-} elseif (isset($_GET["query"])) {
-    $query = filter_input(INPUT_GET, "query", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $tasks = getTasksByQuery($con, $userId, $query);
-} elseif ($currentDay === 1) {
-    $tasks = getTasksByDate($con, $userId, date_create()->format("Y-m-d"));
-    $btnActive["current_day"] = $currentDay;
-} elseif ($tomorrow === 1) {
-    $tasks = getTasksByDate($con, $userId, date_create()->modify("+1 day")->format("Y-m-d"));
-    $btnActive["tomorrow"] = $tomorrow;
-} elseif ($overdue === 1) {
-    $tasks = getOverdueTasks($con, $userId, date_create()->format("Y-m-d"));
-    $btnActive["overdue"] = $overdue;
-} else {
-    $tasks = getTasksAll($con, $userId);
-    $btnActive["all_tasks"] = 1;
-}
+$tasks = getTasksWrapper($con, $userId, $queryStringsValues);
 
-$isProjectExist = in_array($projectId, array_column($projects, "id"));
+$isProjectExist = in_array($queryStringsValues["project_id"], array_column($projects, "id"));
 
-if ($projectId !== null && $isProjectExist === false) {
+if ($queryStringsValues["project_id"] && $isProjectExist === false) {
     http_response_code(404);
     exit();
 }
@@ -76,7 +44,7 @@ if ($projectId !== null && $isProjectExist === false) {
 $projectsSideTemplate = includeTemplate("projects-side.php", [
     "projects" => $projects,
     "scriptName" => pathinfo(__FILE__, PATHINFO_BASENAME),
-    "projectId" => $projectId
+    "projectId" => $queryStringsValues["project_id"]
 ]);
 
 $pageContent = includeTemplate("main.php", [
@@ -84,7 +52,7 @@ $pageContent = includeTemplate("main.php", [
     "tasks" => $tasks,
     "showCompleteTasks" => $showCompleteTasks,
     "scriptName" => pathinfo(__FILE__, PATHINFO_BASENAME),
-    "btnActive" => $btnActive
+    "btnActive" => $queryStringsValues
 ]);
 
 $layoutContent = includeTemplate("layout.php", [
