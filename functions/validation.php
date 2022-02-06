10272b30b60e07cf1d18d178dea643aed4e11715
spook-work <hersgori4@protonmail.com>
2022-02-06 15:57:54 +0300

    created new validateProjectName function and add call of this function in validateProjectForm

--- functions/validation.php
+++ functions/validation.php
@@ -79,6 +79,27 @@ function validateTaskName(string $value): ?string
 }
 
 /**
+ * Проверяет строку в имени проекта на пустоту.
+ * Возвращает сообщение об ошибке или null
+ * @param string $value строка из формы
+ * @return string|null сообщение об ошибке или null
+ */
+function validateProjectName(string $value): ?string
+{
+    $valueLen = mb_strlen(trim($value));
+
+    if ($valueLen == 0) {
+        return "Поле название надо заполнить";
+    }
+
+    if ($valueLen > 255) {
+        return "Название не должно превышать размер в 255 символов";
+    }
+
+    return null;
+}
+
+/**
  * Проверяет является ли выбранное
  * имя проекта существующим для этого пользователя.
  * Возвращает сообщение об ошибке или null
@@ -276,10 +297,12 @@ function validateAuthForm(array $authForm, mysqli $con): array
  */
 function validateProjectForm(array $projectForm, mysqli $con): array
 {
-    $isProjectInDB = isProjectExistsInDB($con, $projectForm["project_name"], $projectForm["user_id"]);
     $errors = [];
 
-    if ($isProjectInDB === true) {
+    $errors["project_name"] = validateProjectName($projectForm["project_name"]);
+    $isProjectInDB = isProjectExistsInDB($con, $projectForm["project_name"], $projectForm["user_id"]);
+
+    if ($isProjectInDB === true && $errors["project_name"] === null) {
         $errors["project_name"] =
             "Данный проект уже добавлен для этого пользователя";
     }
